0e1dc11e4d7885d275ed780b6c072206a23d68f2
Will Brennan <willfbren@gmail.com>
2020-02-22 19:09:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll"
+  puts any_phrase
+end
